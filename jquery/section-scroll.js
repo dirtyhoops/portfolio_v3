4189af6211640a56c2b11f2533263817553d6d49
Daryll  Osis <dosis@csumb.edu>
2020-02-27 23:29:19 -0800

    added a navigation bar

--- jquery/section-scroll.js
+++ jquery/section-scroll.js
@@ -1,4 +1,10 @@
-// The page navigates to the section onClick
+// The page navigates to the sections onClick
+$(document).ready(function() {
+  $('#link-home').click(function() {
+    $path = $('#header').offset().top;
+    $('html, body').animate({ scrollTop: $path }, 850);
+  });
+});
 
 $(document).ready(function() {
   $('#button-header').click(function() {
@@ -7,12 +13,33 @@ $(document).ready(function() {
   });
 });
 
-// For NavBar
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('.nav').addClass('shrink');
+$(document).ready(function() {
+  $('#link-about').click(function() {
+    $path = $('#section-about').offset().top;
+    $('html, body').animate({ scrollTop: $path }, 850);
+  });
+});
+
+$(document).ready(function() {
+  $('#link-project').click(function() {
+    $path = $('#section-project').offset().top;
+    $('html, body').animate({ scrollTop: $path }, 850);
+  });
+});
+
+$(document).ready(function() {
+  $('#link-contact').click(function() {
+    $path = $('#section-contact').offset().top;
+    $('html, body').animate({ scrollTop: $path }, 850);
+  });
+});
+
+// The NavBar darkens on the very first scroll
+$(window).on('scroll', function() {
+  if ($(window).scrollTop() > 20) {
+    $('nav').addClass('dark');
   } else {
-    $('.nav').removeClass('shrink');
+    $('nav').removeClass('dark');
   }
 });
 
@@ -27,13 +54,4 @@ $(document).ready(function() {
   $('.nav__menu ul li a').on('click', function() {
     $('nav ul').toggleClass('showing');
   });
-});
-
-// scrolling effect for nav bar
-$(window).on('scroll', function() {
-  if ($(window).scrollTop() > 20) {
-    $('nav').addClass('black');
-  } else {
-    $('nav').removeClass('black');
-  }
 });
