a53ee43f99a4e2a51911812c260b1e3bcfd6a6ec
Daryll  Osis <dosis@csumb.edu>
2020-02-27 18:35:23 -0800

    just added some images to the project box and about me profile image

--- jquery/section-scroll.js
+++ jquery/section-scroll.js
@@ -6,3 +6,34 @@ $(document).ready(function() {
     $('html, body').animate({ scrollTop: $path }, 850);
   });
 });
+
+// For NavBar
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('.nav').addClass('shrink');
+  } else {
+    $('.nav').removeClass('shrink');
+  }
+});
+
+// for navigation bar when it's mobile size
+$(document).ready(function() {
+  $('.nav__menuicon').on('click', function() {
+    $('nav ul').toggleClass('showing');
+  });
+});
+
+$(document).ready(function() {
+  $('.nav__menu ul li a').on('click', function() {
+    $('nav ul').toggleClass('showing');
+  });
+});
+
+// scrolling effect for nav bar
+$(window).on('scroll', function() {
+  if ($(window).scrollTop() > 20) {
+    $('nav').addClass('black');
+  } else {
+    $('nav').removeClass('black');
+  }
+});
