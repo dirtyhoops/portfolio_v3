4789932f6de96b3e93dd28d9f2334596530431b0
Daryll  Osis <dosis@csumb.edu>
2020-02-29 22:19:52 -0800

    added stylings and animation to the form

---
+++ jquery/form-control.js
@@ -0,0 +1,39 @@
+// Adds the 'active' class when the user focus in the input
+$(document).ready(function() {
+  $('.form-input').on('focusin', function() {
+    $(this)
+      .parent()
+      .find('label')
+      .addClass('active');
+  });
+});
+
+// Removes the 'active' class when the use focous out of the input
+$(document).ready(function() {
+  $('.form-input').on('focusout', function() {
+    $(this)
+      .parent()
+      .find('label')
+      .removeClass('active');
+  });
+});
+
+// Adds the 'active' class when the user focus in the textarea
+$(document).ready(function() {
+  $('.form-textarea').on('focusin', function() {
+    $(this)
+      .parent()
+      .find('label')
+      .addClass('active');
+  });
+});
+
+// Removes the 'active' class when the use focous out of the textarea
+$(document).ready(function() {
+  $('.form-textarea').on('focusout', function() {
+    $(this)
+      .parent()
+      .find('label')
+      .removeClass('active');
+  });
+});
